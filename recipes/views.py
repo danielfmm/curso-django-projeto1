c1af8a372ad6f5ab3e7b8bb87e3b9d7b161a1364
Daniel Ferraz <danielferrazmm@hotmail.com>
2023-02-25 02:27:00 -0300

    Dividindo em page e partials

--- recipes/views.py
+++ recipes/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Daniel Ferraz',
+    })
