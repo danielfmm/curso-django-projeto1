1fb963d231898f0aba06f3ea548c016c2414d1fb
Daniel Ferraz <danielferrazmm@hotmail.com>
2023-02-25 01:00:50 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,7 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html', using='')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
+    return render(request, 'recipes/home.html')
