f8ba0a2670d1206f56f174f3263436171867145d
Daniel Ferraz <danielferrazmm@hotmail.com>
2023-02-28 19:50:38 -0300

    updates #1

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,16 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Daniel Ferraz',
+        'recipes': [make_recipe() for _ in range(10)]
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe()
     })
